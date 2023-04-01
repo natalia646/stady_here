93a388d5a23132754b77af78b738728cb2c0a1dd
natalia646 <nataxash46@gmail.com>
2023-04-01 18:28:23 +0300

    oop

--- js/index.js
+++ js/index.js
@@ -1,45 +1,21 @@
 class User {
-  constructor(name, age, status) {
-    this.name = name;
+  constructor(name, age, status, course) {
+    this._name = name;
     this.age = age;
     this.status = status;
-  }
-
-  greting() {
-    return `Hi ${this.name}`;
+    this.course = course;
   }
 }
 
 const user1 = new User("rew", 43, true);
-const user2 = new User("pit", 23, false);
-class Compere {
-  static equal(a, b) {
-    return a === b;
-  }
+const user2 = new User("pit", 23, false, 6);
 
-  get studentCourse() {
-    return this.course;
-  }
+let descriptor = Object.getOwnPropertyDescriptor(user1, "age");
+console.log(descriptor);
 
-  static less(a, b) {
-    return a < b;
-  }
-
-  static half(n) {
-    return n / 2;
-  }
-}
-
-console.log(Compere.equal(5, 7));
-console.log(Compere.more(user1.age, user2.age));
-
-
-class Counting extends Compere{
-  static count (...arg){
-    return arg.reduce((acum, n) => acum += n )
-  }
-}
-
-console.log(Counting.count(4, 6, 8, 9, 3));
-console.log(Counting.equal(4, 6, 8, 9, 3));
-console.log(Compere.count(4, 6, 8, 9, 3)); // false
+Object.defineProperty = (user2, 'age', {
+  configurable: true,
+  enumerable: true,
+  value: 43,
+  writable: true,
+});
