aeb5cc06564ff58b8512de87c178f680744f97fb
natalia646 <nataxash46@gmail.com>
2023-04-01 18:43:18 +0300

    define property

--- js/index.js
+++ js/index.js
@@ -10,12 +10,25 @@ class User {
 const user1 = new User("rew", 43, true);
 const user2 = new User("pit", 23, false, 6);
 
-let descriptor = Object.getOwnPropertyDescriptor(user1, "age");
+let descriptor = Object.getOwnPropertyDescriptor(user1, "age"); // дозволяє отримати повну інформацію про властивість обєкта(Властивості об’єкта, крім value, мають три спеціальні атрибути (так звані “прапори”):)
 console.log(descriptor);
 
-Object.defineProperty = (user2, 'age', {
-  configurable: true,
-  enumerable: true,
-  value: 43,
-  writable: true,
-});
+Object.defineProperty =
+  (user2,
+  "age",
+  {
+    configurable: true, // true/false --- дозволяє/забороняє зміну чи видалення прапорів властивості, однак дозволяє перевизначати властивость
+    enumerable: true, // true/false --- дозволяє/забороняє бути штерованим в циклі (тобто даний ключ чи значення буде злічуватися циклом чи ні)
+    value: 43,
+    writable: true, // true/false --- дозволяє/забороняє перевизначати властивість
+  });
+
+let mathPi = Object.getOwnPropertyDescriptor(Math, "PI");
+console.log(mathPi);
+
+const mathPiDesc = {
+  configurable: false,
+  enumerable: false,
+  value: 3.141592653589793,
+  writable: false,
+}; // тобто в обєкті(класс) Math є ключ PI який, не можна перевизначати, не можна видалаяти чи змінювати будь-який прапорекь цієї властивості, та не ітерується
--- js/oop.js
+++ js/oop.js
@@ -200,3 +200,26 @@ function Car(make, modal, prise) {
   const showPYear2 = porschPanamera.year.bind(fordMustang);
   console.log(showPYear2(2018, "December"));
   
+ //! getOwnPropertyDescriptor , defineProperty
+  let descriptor = Object.getOwnPropertyDescriptor(user1, "age"); // дозволяє отримати повну інформацію про властивість обєкта(Властивості об’єкта, крім value, мають три спеціальні атрибути (так звані “прапори”):)
+console.log(descriptor);
+
+Object.defineProperty =
+  (user2,
+  "age",
+  {
+    configurable: true, // true/false --- дозволяє/забороняє зміну чи видалення прапорів властивості, однак дозволяє перевизначати властивость
+    enumerable: true, // true/false --- дозволяє/забороняє бути штерованим в циклі (тобто даний ключ чи значення буде злічуватися циклом чи ні)
+    value: 43,
+    writable: true, // true/false --- дозволяє/забороняє перевизначати властивість
+  });
+
+let mathPi = Object.getOwnPropertyDescriptor(Math, "PI");
+console.log(mathPi);
+
+const mathPiDesc = {
+  configurable: false,
+  enumerable: false,
+  value: 3.141592653589793,
+  writable: false,
+}; // тобто в обєкті(класс) Math є ключ PI який, не можна перевизначати, не можна видалаяти чи змінювати будь-який прапорекь цієї властивості, та не ітерується
