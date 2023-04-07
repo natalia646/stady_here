99850beb6fd11ecbb1f6d9e4cb313e00675495ff
natalia646 <nataxash46@gmail.com>
2023-04-07 11:03:04 +0300

    asing

--- js/oop.js
+++ js/oop.js
@@ -175,6 +175,32 @@ SuperPuperCarr.prototype.cary = function () {
   console.log(Counting.equal(4, 6, 8, 9, 3));
   console.log(Compere.count(4, 6, 8, 9, 3)); // false
 
+  class Student {
+    
+    constructor(fullName, direction) {
+      this._fullName = fullName;
+      this._direction = direction;
+    }
+    showFullName(){
+        return this._fullName
+    }
+    nameIncludes(data){
+        return this.showFullName().includes(data)
+    }
+    static studentBuilder(){
+        return new Student('Ihor Kohut', 'qc')
+    }
+    get direction(){
+        return this._direction
+    }
+    set direction(value){
+      this._direction = value 
+    }
+  }
+  
+  const stud1 = new Student('Ivan Petrenko', 'web');
+  const stud2 = new Student('Sergiy Koval', 'python');
+  const stud3 = Student.studentBuilder();
 
   //! apply, call, bind
 function Car(make, modal, prise) {
